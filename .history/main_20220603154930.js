fbeac0c52342f0b3caef0e5360e0bcc2019ca4a1
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-06-03 15:56:00 -0400

    random generation of stars

--- .history/main_20220603154930.js
+++ .history/main_20220603154930.js
@@ -37,17 +37,14 @@ scene.add(lightHelper, gridHelper);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar () {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const geometry = new THREE.SphereGeometry(0, 25, 24, 24);
   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh( geometry, material );
+  const sphere = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+  const [x, y, z] = Array(3).fill().map(() => Math.random() * 100 - 50);
 
-  star.position.set(x, y, z);
-  scene.add(star);
+  scene.add(sphere);
 }
-
-Array(200).fill().forEach(addStar);
 
 function animate () {
   requestAnimationFrame(animate);
