b8e819537a513a21e4fc664005d94791d447d817
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-06-03 15:18:22 -0400

    torus rendered

--- .history/main_20220603151511.js
+++ .history/main_20220603151511.js
@@ -16,10 +16,4 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({ color: 0xff6347, wireframe: true });
-const torus = new THREE.Mesh(geometry, material);
-
-scene.add(torus);
-
-renderer.render(scene, camera);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
